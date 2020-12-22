214e3fce8796641f8e5a70375a515890c165dd47
Brian Brown <bb.brown@mac.com>
2020-12-21 20:53:30 -0800

    techproject push again
    pushing for help again

--- techproject/src/mainapp/products/views.py
+++ techproject/src/mainapp/products/views.py
@@ -1,3 +1,10 @@
+from django.shortcuts import render, get_object_or_404, redirect
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+
+def admin_console(request):
+    products = Product.objects.all()
+    return render(request, 'products/products_page.html', {'products': products})
