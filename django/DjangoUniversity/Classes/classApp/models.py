149f3a880f7191284fc83dda7ab9e1602ac72a5f
Brian Brown <bb.brown@mac.com>
2020-12-29 18:40:58 -0800

    Django Challenge Part 2 push

--- django/DjangoUniversity/Classes/classApp/models.py
+++ django/DjangoUniversity/Classes/classApp/models.py
@@ -8,3 +8,7 @@ class djangoClasses(models.Model):
     InstructorName = models.CharField(max_length=50, default='')
     Duration = models.FloatField(max_length=50, default='')
     course = models.Manager()
+    def __str__(self):
+        return self.Title
+
+#object1 = djangoClasses.course.create(Title = "English 100", CourseNumber = 100, InstructorName = "Bob Dole", Duration = 1.5)
