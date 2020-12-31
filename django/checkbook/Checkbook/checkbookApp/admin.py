94a01bef1ef934e7dd428b3afbf2b8ebf880ab26
Brian Brown <bb.brown@mac.com>
2020-12-31 15:49:19 -0800

    checkbook 2nd push

--- django/checkbook/Checkbook/checkbookApp/admin.py
+++ django/checkbook/Checkbook/checkbookApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import checkbookApp
+from .models import User, Transactions
 
 # Register your models here.
 
-admin.site.resister(checkbookApp)
+#admin.site.register(checkbookApp)
+admin.site.register(User)
+admin.site.register(Transactions)
--- django/checkbook/Checkbook/checkbookApp/models.py
+++ django/checkbook/Checkbook/checkbookApp/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+#from . import checkbookApp
 
 # Create your models here.
 
 class User(models.Model):
     FirstName = models.CharField(max_length=50, default="")
     LastName = models.CharField(max_length=50, default="")
-    StartingBalance = models.DecimalField(max_length=50, default="", decimal_places=2)
+    StartingBalance = models.DecimalField(max_digits=50, default="", decimal_places=2)
 
 class Transactions(models.Model):
     TransactionDate = models.DateField(max_length=50, default="")
     TransactionType = models.CharField(max_length=50, default="")
-    Amount = models.DecimalField(max_length=50, default="", decimal_places=2)
-    Account = models.TextChoices(max_length=50, default="")
+    Amount = models.DecimalField(max_digits=50, default="", decimal_places=2)
+    Account = models.CharField(max_length=50, default="")
     trans = models.Manager()
 
