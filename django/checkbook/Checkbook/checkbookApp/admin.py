5974206573bd03a05a2639dcd1cfa72b1dceb6c2
Brian Brown <bb.brown@mac.com>
2020-12-31 20:41:18 -0800

    turn in django checkbook push

--- django/checkbook/Checkbook/checkbookApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Transactions
-
-# Register your models here.
-
-#admin.site.register(checkbookApp)
-admin.site.register(User)
-admin.site.register(Transactions)
